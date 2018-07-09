82ab59232487e39ebdd2583df9c7b58399e51488
Taschee <sedlmayr_alex@gmx.de>
2018-07-09 17:06:02 +0200

    Bugfix sampling of opponent cards not depending on game object. now returns list of all playerhands, not only opponents

--- schafkopf/helpers.py
+++ schafkopf/helpers.py
@@ -1,14 +1,17 @@
 import random
 
 
-def sample_opponent_cards(tricks, current_trick, trumpcards, playerindex, player_hand):
+def sample_opponent_hands(tricks, current_trick, trumpcards, playerindex, player_hand):
     opp_cards = opponent_cards_still_in_game(tricks, current_trick, player_hand)
+
     while True:
         random.shuffle(opp_cards)
-        print("Opponent cards : ", opp_cards)
-        if card_distribution_possible(tricks, current_trick, trumpcards, playerindex, opp_cards):
+        sample_hands = deal_player_hands(playerindex=playerindex, opp_card_distribution=opp_cards,
+                                         tricks=tricks, current_trick=current_trick)
+        if card_distribution_possible(tricks, current_trick, trumpcards, playerindex, sample_hands):
             break
-    return opp_cards
+
+    return sample_hands
 
 
 def opponent_cards_still_in_game(tricks, current_trick, player_hand):
@@ -21,11 +24,31 @@ def opponent_cards_still_in_game(tricks, current_trick, player_hand):
     return list(opp_cards)
 
 
+def deal_player_hands(opp_cards, playerindex, player_hand, tricks, current_trick):
+    opp_indices = list({0, 1, 2, 3})
+    opp_indices.remove(playerindex)
+
+    player_hands = [None, None, None, None]
+    player_hands[playerindex] = player_hand
+
+    for opp_index in opp_indices:
+
+        number_of_cards = 8 - len(tricks)
+        if current_trick.cards[opp_index] is not None:
+            number_of_cards -= 1
+        opp_hand = opp_cards[:number_of_cards]
+        opp_cards = opp_cards[number_of_cards:]
+        player_hands[opp_index] = opp_hand
+
+    return player_hands
+
+
 def didnt_follow_trump(tricks, current_trick, trumpcards, playerindex):
     for trick in tricks + [current_trick]:
         first_card = trick.cards[trick.leading_player_index]
         if first_card in trumpcards and first_card is not None:
-            if trick.cards[playerindex] not in trumpcards:
+            playercard = trick.cards[playerindex]
+            if playercard not in trumpcards and playercard is not None:
                 return True
     else:
         return False
@@ -37,31 +60,29 @@ def suits_not_followed(tricks, current_trick, trumpcards, playerindex):
         first_card = trick.cards[trick.leading_player_index]
         if first_card not in trumpcards and first_card is not None:
             suit = first_card[1]
-            if trick.cards[playerindex][1] != suit:
+            played_card = trick.cards[playerindex]
+            if played_card[1] != suit and played_card is not None:
                 missing_suits.append(suit)
     return missing_suits
 
 
-def card_distribution_possible(tricks, current_trick, trumpcards, playerindex, opp_card_distribution):
+def card_distribution_possible(tricks, current_trick, trumpcards, playerindex, player_hands):
 
-    opp_indices = list({0, 1, 2, 3})
+    opp_indices = {0, 1, 2, 3}
     opp_indices.remove(playerindex)
+    opp_indices = list(opp_indices)
 
     for opp_index in opp_indices:
 
-        number_of_cards = 8 - len(tricks)
-        if current_trick.cards[opp_index] is not None:
-            number_of_cards -= 1
-        opp_cards = set(opp_card_distribution[:number_of_cards])
-        opp_card_distribution = opp_card_distribution[number_of_cards:]
+        opp_cards = set(player_hands[opp_index])
 
         if didnt_follow_trump(tricks, current_trick, trumpcards, opp_index):
-            if len(opp_cards & trumpcards) > 0:
+            if len(opp_cards & set(trumpcards)) > 0:
                 return False
 
         for suit in suits_not_followed(tricks, current_trick, trumpcards, opp_index):
             for card in opp_cards:
-                if card[1] == suit:
+                if card[1] == suit and card not in trumpcards:
                     return False
 
     return True
---
+++ tests/test_helpers.py
@@ -0,0 +1,89 @@
+import pytest
+from schafkopf.game import Game, Trick
+from schafkopf.players import RandomPlayer
+import schafkopf.helpers as hlp
+
+SIEBEN = 0
+ACHT = 1
+NEUN = 2
+UNTER = 3
+OBER = 4
+KOENIG = 5
+ZEHN = 6
+AS = 7
+
+SCHELLEN = 0
+HERZ = 1
+GRAS = 2
+EICHEL = 3
+SUITS = [EICHEL, GRAS, HERZ, SCHELLEN]
+
+# every game mode is a tuple (game_type, suit). possible game_types are:
+WEITER = 0
+RUFSPIEL = 1
+WENZ = 2
+SOLO = 3
+
+
+trumpcards = [(OBER, i) for i in SUITS] + [(UNTER, i) for i in SUITS] \
+                                + [(AS, HERZ), (ZEHN, HERZ), (KOENIG, HERZ), (NEUN, HERZ), (ACHT, HERZ), (SIEBEN, HERZ)]
+
+first_trick = Trick(leading_player_index=0)
+first_trick.cards = [(NEUN, HERZ), (UNTER, EICHEL), (OBER, HERZ), (ZEHN, SCHELLEN)]
+first_trick.score = 15
+first_trick.winner = 2
+sec_trick = Trick(leading_player_index=2)
+sec_trick.cards = [(AS, GRAS), (AS, HERZ), (AS, EICHEL), (SIEBEN, EICHEL)]
+sec_trick.score = 26
+sec_trick.winner = 1
+
+tricks = [first_trick, sec_trick]
+
+current_trick = Trick(leading_player_index=1)
+current_trick.cards = [None, (UNTER, SCHELLEN), (ZEHN, GRAS), None]
+
+playerindex = 1
+player_hand = [(ZEHN, HERZ), (OBER, GRAS), (UNTER, GRAS), (SIEBEN, GRAS), (OBER, SCHELLEN)]
+
+
+def test_opp_cards_still_in_game():
+    opp_cards = hlp.opponent_cards_still_in_game(tricks=tricks,
+                                                 current_trick=current_trick,
+                                                 player_hand=player_hand)
+    assert set(opp_cards) == {(OBER, EICHEL), (UNTER, HERZ), (SIEBEN, HERZ), (ACHT, HERZ),
+                              (KOENIG, HERZ), (ACHT, EICHEL), (NEUN, EICHEL), (ZEHN, EICHEL), (AS, SCHELLEN),
+                              (KOENIG, SCHELLEN), (SIEBEN, SCHELLEN), (ACHT, SCHELLEN), (NEUN, SCHELLEN),
+                              (KOENIG, GRAS), (KOENIG, EICHEL), (ACHT, GRAS), (NEUN, GRAS)}
+
+
+def test_suit_not_followed():
+    assert hlp.suits_not_followed(tricks, current_trick, trumpcards, playerindex=0) == [EICHEL]
+    assert hlp.suits_not_followed(tricks, current_trick, trumpcards, playerindex=1) == [EICHEL]
+    assert hlp.suits_not_followed(tricks, current_trick, trumpcards, playerindex=2) == []
+
+
+def test_didnt_follow_trump():
+    assert hlp.didnt_follow_trump(tricks, current_trick, trumpcards, playerindex=2)
+    assert hlp.didnt_follow_trump(tricks, current_trick, trumpcards, playerindex=3)
+    assert not hlp.didnt_follow_trump(tricks, current_trick, trumpcards, playerindex=1)
+
+
+def test_distribution_possible():
+
+    poss_hands = [[(OBER, EICHEL), (UNTER, HERZ), (SIEBEN, HERZ), (ACHT, HERZ), (KOENIG, HERZ), (ACHT, GRAS)],
+                  player_hand,
+                  [(SIEBEN, SCHELLEN), (ACHT, SCHELLEN), (NEUN, SCHELLEN), (KOENIG, GRAS), (KOENIG, EICHEL)],
+                  [(ACHT, EICHEL), (NEUN, EICHEL), (ZEHN, EICHEL), (AS, SCHELLEN), (KOENIG, SCHELLEN), (NEUN, GRAS)]]
+
+    assert hlp.card_distribution_possible(tricks=tricks, current_trick=current_trick, player_hands=poss_hands,
+                                          playerindex=playerindex, trumpcards=trumpcards)
+
+    not_poss_hands = [[(SIEBEN, SCHELLEN), (NEUN, EICHEL), (SIEBEN, HERZ), (ACHT, HERZ), (KOENIG, HERZ), (ACHT, GRAS)],
+                      player_hand,
+                      [(OBER, EICHEL), (ACHT, SCHELLEN), (NEUN, SCHELLEN), (KOENIG, GRAS), (KOENIG, EICHEL)],
+                      [(ACHT, EICHEL), (UNTER, HERZ), (ZEHN, EICHEL), (AS, SCHELLEN), (KOENIG, SCHELLEN), (NEUN, GRAS)]]
+
+    assert not hlp.card_distribution_possible(tricks=tricks, current_trick=current_trick,
+                                              player_hands=not_poss_hands, playerindex=playerindex,
+                                              trumpcards=trumpcards)
+
