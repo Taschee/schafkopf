f12fbaf79c76220595c44852ce530151dcd16c69
Taschee <sedlmayr_alex@gmx.de>
2018-08-10 11:16:06 +0200

    tree visualization, filename added

--- schafkopf/mc_tree_analizing/mc_tree_visualization.py
+++ schafkopf/mc_tree_analizing/mc_tree_visualization.py
@@ -6,7 +6,11 @@ from schafkopf.game_states_trick_play import sample_game_states
 
 def main():
     game_state = sample_game_states[10]
-    player = UCTPlayer(ucb_const=0, num_samples=1, num_simulations=100)
+
+    num_simulations = 100
+    ucb_constant = 0.1
+
+    player = UCTPlayer(ucb_const=ucb_constant, num_samples=1, num_simulations=num_simulations)
 
     root_node = MCNode(game_state=game_state)
     mc_tree = MCTree(root_node=root_node)
@@ -16,8 +20,10 @@ def main():
         rewards = player.simulation(selected_node)
         mc_tree.backup_rewards(leaf_node=selected_node, rewards=rewards)
 
-    mc_tree.visualize_tree(ucb=player.ucb_const, filename="Try")
-
+    mc_tree.visualize_tree(ucb=player.ucb_const,
+                           filename="Tree_{}nodes{}ucb_const{}game_mode".format(num_simulations,
+                                                                                ucb_constant,
+                                                                                game_state["game_mode"]))
 
 if __name__ == "__main__":
     main()
