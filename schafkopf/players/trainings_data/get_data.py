dd5d0da584c1c5247876fe69e96304f223ca784e
Taschee <sedlmayr_alex@gmx.de>
2018-08-27 22:31:24 +0200

    created trick game for trick playing phase

--- schafkopf/players/trainings_data/get_data.py
+++ schafkopf/players/trainings_data/get_data.py
@@ -7,15 +7,22 @@ scraper = DataScraper()
 
 game_numbers = [846389616, 846389972, 846389815, 846389389, 846389142]
 
+
 with open('data_test.p', 'wb') as outfile:
 
     for num in game_numbers:
-        player_hands, game_mode, declaring_player, played_cards = scraper.scrape(game_num=num)
 
-        data = {'player_hands': player_hands,
-                'game_mode': game_mode,
-                'declaring_player': declaring_player,
-                'played_cards': played_cards}
+        username = input("Username : ")
+        password = input("Password : ")
 
-        pickle.dump(data, outfile)
+        player_hands, game_mode, declaring_player, played_cards = scraper.scrape(num, username, password)
+
+        if game_mode is not None:
+
+            data = {'player_hands': player_hands,
+                    'game_mode': game_mode,
+                    'declaring_player': declaring_player,
+                    'played_cards': played_cards}
+
+            pickle.dump(data, outfile)
 
