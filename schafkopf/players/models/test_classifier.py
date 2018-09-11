0bca4da615508faf9ef7ec75965d131cafd5c8ec
Taschee <sedlmayr_alex@gmx.de>
2018-09-11 11:37:19 +0200

    quick testing of classifier

---
+++ schafkopf/players/models/test_classifier.py
@@ -0,0 +1,31 @@
+import keras
+import numpy as np
+from schafkopf.ranks import SEVEN, EIGHT, NINE, TEN, UNTER, OBER, KING, ACE
+from schafkopf.suits import BELLS, ACORNS, HEARTS, LEAVES
+import schafkopf.players.data.encodings as enc
+
+
+filepath = "bigger_classifier_sortedhands_lr0.02.hdf5"
+
+model = keras.models.load_model(filepath)
+
+hand_wenz = enc.encode_one_hot_hand([(UNTER, ACORNS), (UNTER, HEARTS), (UNTER, BELLS), (ACE, LEAVES),
+                                     (EIGHT, LEAVES), (ACE, ACORNS), (EIGHT, BELLS), (SEVEN, BELLS)])
+
+hand_solo_leaves = enc.encode_one_hot_hand([(OBER, ACORNS), (OBER, HEARTS), (OBER, LEAVES), (UNTER, ACORNS),
+                                            (UNTER, BELLS), (TEN, LEAVES), (NINE, LEAVES), (ACE, HEARTS)])
+
+hand_partner_leaves = enc.encode_one_hot_hand([(OBER, ACORNS), (OBER, HEARTS), (UNTER, BELLS), (ACE, HEARTS),
+                                              (NINE, HEARTS), (EIGHT, BELLS), (TEN, LEAVES), (ACE, ACORNS)])
+
+hand_no_game = enc.encode_one_hot_hand([(OBER, ACORNS), (KING, HEARTS), (ACE, BELLS), (TEN, BELLS),
+                                        (EIGHT, BELLS), (SEVEN, BELLS), (KING, ACORNS), (NINE, ACORNS)])
+
+hands = np.array([hand_wenz, hand_solo_leaves, hand_partner_leaves, hand_no_game])
+
+print(model.predict(np.array([hand_wenz])))
+print(model.predict(np.array([hand_solo_leaves])))
+print(model.predict(np.array([hand_partner_leaves])))
+print(model.predict(np.array([hand_no_game])))
+
+print(model.predict_classes(hands))
