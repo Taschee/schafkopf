9e5166e188fca291088d3b59bab6c3c194c02fb4
Taschee <sedlmayr_alex@gmx.de>
2018-09-25 10:42:54 +0200

    fix

--- schafkopf/players/__init__.py
+++ schafkopf/players/__init__.py
@@ -1,6 +1,5 @@
 from schafkopf.players.random_player import RandomPlayer
+from schafkopf.players.dummy_player import DummyPlayer
 from schafkopf.players.heuristics_player import HeuristicsPlayer
 from schafkopf.players.uct_player import UCTPlayer
-from schafkopf.players.nn_player import NNPlayer
 from schafkopf.players.human_console_player import HumanConsolePlayer
-from schafkopf.players.dummy_player import DummyPlayer
