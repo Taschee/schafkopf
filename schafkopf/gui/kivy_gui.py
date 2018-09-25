442083bf84cc103e512bb701413f5bd60cbf7d56
Taschee <sedlmayr_alex@gmx.de>
2018-09-25 11:32:40 +0200

    kivy GUI move all game stuff to playing screen

--- schafkopf/gui/kivy_gui.py
+++ schafkopf/gui/kivy_gui.py
@@ -6,19 +6,20 @@ from schafkopf.game import Game
 from schafkopf.helpers import sort_hand
 from schafkopf.suits import *
 from schafkopf.game_modes import *
+from schafkopf.players import RandomPlayer, HumanConsolePlayer, DummyPlayer
 
 from kivy.app import App
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 
 SUITS = {"0": "Schellen", "1": "Herz", "2": "Gras", "3": "Eichel"}
 SYMBOLS = {"0": "7", "1": "8", "2": "9", "3": "U", "4": "O", "5": "K", "6": "10", "7": "A"}
 
 
-class MyScreenManager(ScreenManager):
+class GameScreenManager(ScreenManager):
 
     def __init__(self, playerlist):
         ScreenManager.__init__(self)
@@ -27,15 +28,30 @@ class MyScreenManager(ScreenManager):
         self.current_game_state = None
 
     def new_game(self):
+        screen = self.get_screen('playing_screen')
+        screen.new_game()
+        self.current = 'playing_screen'
+
+
+class MenuScreen(Screen):
+    pass
+
+
+class PlayingScreen(Screen):
+    def __init__(self, **kwargs):
+        Screen.__init__(self, **kwargs)
+        self.leading_player_index = random.choice(range(4))
+        self.playerlist = None
+        self.current_game_state = None
+
+    def new_game(self):
         player_hands = CardDeck().shuffle_and_deal_hands()
         player_index = 0
         hand = sort_hand(player_hands[player_index])
-        screen = self.get_screen('playing_screen')
-        for card, widget in zip(hand, screen.ids.cards.children):
+        for card, widget in zip(hand, self.ids.cards.children):
             im_name = SYMBOLS[str(card[0])] + SUITS[str(card[1])] + ".jpg"
             filepath = PurePath('..', 'images', im_name)
             widget.source = str(filepath)
-        self.current = 'playing_screen'
         self.current_game_state = self.new_game_state(player_hands)
 
     def new_game_state(self, player_hands):
@@ -50,12 +66,6 @@ class MyScreenManager(ScreenManager):
                       'current_trick': None}
         return game_state
 
-
-class MenuScreen(Screen):
-    pass
-
-
-class PlayingScreen(Screen):
     def print_msg(self, string):
         print(string)
 
@@ -92,8 +102,8 @@ class CardWidget(GridLayout):
 
 class SchafkopfApp(App):
     def build(self):
-        playerlist = []
-        return MyScreenManager(playerlist)
+        playerlist = [HumanConsolePlayer(), RandomPlayer(), RandomPlayer(), RandomPlayer()]
+        return GameScreenManager(playerlist)
 
 
 if __name__ == '__main__':
