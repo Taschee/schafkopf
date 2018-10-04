cd9284efd3fd0c98577155833c9e9a8f5b809273
Taschee <sedlmayr_alex@gmx.de>
2018-10-04 12:39:10 +0200

    wider model

--- schafkopf/players/models/train_trickplay.py
+++ schafkopf/players/models/train_trickplay.py
@@ -17,18 +17,27 @@ def load_data(file):
 
 def lstm_model():
     model = Sequential()
-    model.add(LSTM(256, input_shape=(28, 36)))
+    model.add(LSTM(250, input_shape=(28, 36)))
     model.add(Dropout(0.2))
     model.add(Dense(32, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
 
-def bigger_lstm_model():
+def wider_lstm_model():
     model = Sequential()
-    model.add(LSTM(256, input_shape=(28, 36), return_sequences=True))
+    model.add(LSTM(500, input_shape=(28, 36), return_sequences=True))
     model.add(Dropout(0.2))
-    model.add(LSTM(256))
+    model.add(Dense(32, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+def deeper_lstm_model():
+    model = Sequential()
+    model.add(LSTM(250, input_shape=(28, 36), return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(250))
     model.add(Dropout(0.2))
     model.add(Dense(32, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -55,7 +64,8 @@ def main():
     parser.add_argument('data', help='pickle file with the trainings data')
     parser.add_argument('--modelname', help='name of the model to be trained')
     parser.add_argument('--epochs', help='number of training epochs', type=int)
-    parser.add_argument('--bigger_lstm', help='use two lstm blocks', action='store_true')
+    parser.add_argument('--wider_lstm', help='use two lstm blocks of 250 cells', action='store_true')
+    parser.add_argument('--deeper_lstm', help='use two lstm blocks of 250 cells', action='store_true')
     args = parser.parse_args()
 
     x_train, y_train, x_val, y_val, _, _ = load_data(args.data)
@@ -65,8 +75,10 @@ def main():
     else:
         num_epochs = 20
 
-    if args.bigger_lstm:
-        model = bigger_lstm_model()
+    if args.deeper_lstm:
+        model = deeper_lstm_model()
+    elif args.wider_lstm:
+        model = wider_lstm_model()
     else:
         model = lstm_model()
 
