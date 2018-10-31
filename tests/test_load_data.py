7419307a2c40d3cbf60feb11e6f3dd1b76722817
Taschee <sedlmayr_alex@gmx.de>
2018-10-31 16:32:45 +0100

    test data preparation for inference network

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import schafkopf.players.data.load_data as ld
+import schafkopf.players.data.encodings as enc
 from schafkopf.game_modes import PARTNER_MODE
+from schafkopf.ranks import KING
 from schafkopf.suits import BELLS, ACORNS, LEAVES, HEARTS
 
 
@@ -35,3 +37,30 @@ def test_prepare_data_bidding():
     assert y.shape == (9, )
     data_list_augmented = ld.prepare_data_bidding(game_data_dic, augment_data=True)
     assert len(data_list_augmented) == 24
+
+
+def test_prepare_data_inference():
+    card_sequences, hands_to_predict = ld.prepare_data_inference(game_data_dic, num_samples=26)
+    assert len(card_sequences) == 26
+    assert len(hands_to_predict) == 26
+    seq = card_sequences[0]
+    hands = hands_to_predict[0]
+    assert hands.shape == (128, )
+    assert seq.shape == (28, 36)
+    shortest_seq = None
+    pl_hands = None
+    for s, i in zip(card_sequences, range(len(hands_to_predict))):
+        if np.array_equal(s[2][:32], np.zeros(32)):
+            shortest_seq = s
+            pl_hands = hands_to_predict[i]
+    played_card = shortest_seq[1][:32]
+    assert pl_hands.shape == (128, )
+    assert enc.decode_one_hot_card(played_card) == (KING, LEAVES)
+    goal = np.zeros(128)
+    indices = [16, 29, 25, 21, 9, 23, 24, 46, 45, 33, 63, 59, 42, 34, 60, 83,
+               82, 81, 76, 69, 75, 67, 90, 111, 103, 126, 102, 116, 104, 100, 96]
+    for index in indices:
+        goal[index] = 1
+    for i in range(128):
+        assert goal[i] == pl_hands[i], 'failed at card {}'.format(i)
+    assert np.array_equal(goal, pl_hands)
