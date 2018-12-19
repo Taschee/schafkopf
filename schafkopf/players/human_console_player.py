1a0cb68a58e9de902b8daf3952afeec0b9975f46
Taschee <sedlmayr_alex@gmx.de>
2018-12-19 10:06:18 +0100

    estimating number of legal cards to follow

--- schafkopf/players/human_console_player.py
+++ schafkopf/players/human_console_player.py
@@ -5,18 +5,23 @@ class HumanConsolePlayer(Player):
     """Player for playing on console"""
     def choose_game_mode(self, public_info, options):
         while True:
-            mode = input("Hand : {} \n"
+            mode_index = input("Hand : {} \n"
                          "Previous Proposals: {} \n"
-                         "Choose Game Mode : ".format(self.hand, public_info["mode_proposals"]))
+                         "Options : {}"
+                         "Choose Game Mode : ".format(self.hand, public_info["mode_proposals"], options))
+            mode = options[int(mode_index)]
             if mode in options:
                 break
         return mode
 
     def play_card(self, public_info, options=None):
         while True:
-            card = input("Hand : {} \n"
+            card_index = input("Hand : {} \n"
+                         "Previous tricks : {}"
                          "Current Trick : {}\n"
-                         "Play card : ".format(self.hand, public_info["current_trick"]))
+                         "Options : {}" 
+                         "Play card : ".format(self.hand, public_info['tricks'], public_info["current_trick"], options))
+            card = options[int(card_index)]
             if card in self.hand:
                 break
         self.hand.remove(card)
